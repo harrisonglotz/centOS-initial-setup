08d40ebf53f8e14771d03d0113def427e7e81b85
harrisonglotz <harrisonglotz@gmail.com>
2020-05-16 23:23:39 -0400

    edited update.py

--- update.py
+++ update.py
@@ -4,15 +4,17 @@ import os
 import time
 
 #update server
+print('\nGetting updates...\n\n')
 os.system('dnf check-update')
 os.system('dnf update')
 time.sleep(15)
 os.system('y')
 os.system('dnf clean all')
 
+print('\nGetting preferred packages...\n\n')
 #install preferred packages
-os.system('dnf install vim nano wget curl net-tools')
-os.system('')
+os.system('dnf install openssh-server vim nano wget curl net-tools')
+os.system('y')
 
 
 
