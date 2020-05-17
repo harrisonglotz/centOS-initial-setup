4019b4f8064693111d29301d2a4d3a1e05440ed5
harrisonglotz <harrisonglotz@gmail.com>
2020-05-17 12:23:39 -0400

    edited update.py

--- update.py
+++ update.py
@@ -4,17 +4,33 @@ import os
 import time
 
 #update server
-print('\nGetting updates...\n\n')
+print('\n*****Getting updates...*****\n\n')
 os.system('dnf check-update')
 os.system('dnf update')
-time.sleep(15)
-os.system('y')
 os.system('dnf clean all')
 
-print('\nGetting preferred packages...\n\n')
 #install preferred packages
+print('\n*****Getting preferred packages...*****\n\n')
 os.system('dnf install openssh-server vim nano wget curl net-tools')
 os.system('y')
+
+#start SSH
+print('*****Starting SSH...*****')
+os.system('systemctl start ssh')
+os.system('systemctl restart ssh')
+
+#create new sudo user
+print('*****Creating sudo user...*****')
+username = input('Enter username for sudo user: ')
+os.system(f'adduser {username}')
+os.system(f'passwd {username}')
+os.system(f'usermod -aG wheel {username}')
+
+print('*****Set up script complete!*****')
+
+
+
+
 
 
 
